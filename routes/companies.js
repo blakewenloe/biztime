2e18b176a1f14e06ecf600052f93364a094a07cb
Blake Enloe <blakewenloe@yahoo.com>
2020-11-09 16:59:00 -0600

    added route for companies by code

--- routes/companies.js
+++ routes/companies.js
@@ -9,7 +9,22 @@ router.get("/", async function (req, res, next) {
       `SELECT code, name, description FROM companies`
     );
 
-    return res.json(results.rows);
+    return res.json({ companies: results.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+//get a company by its code
+router.get("/:code", async function (req, res, next) {
+  try {
+    const results = await db.query(
+      `SELECT code, name, description
+      FROM companies
+      WHERE code=$1`,
+      [req.params.code]
+    );
+    return res.json({ company: results.rows });
   } catch (err) {
     return next(err);
   }
