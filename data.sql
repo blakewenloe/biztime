651c16b3ff88f4f040f4fb5d45b7ff85044d2701
Blake Enloe <blakewenloe@yahoo.com>
2020-11-09 16:19:06 -0600

    fixed db.js

--- data.sql
+++ data.sql
@@ -1,14 +1,10 @@
-\c biztime
-
-DROP TABLE IF EXISTS invoices;
+\ c biztime DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
-
 CREATE TABLE companies (
     code text PRIMARY KEY,
     name text NOT NULL UNIQUE,
     description text
 );
-
 CREATE TABLE invoices (
     id serial PRIMARY KEY,
     comp_code text NOT NULL REFERENCES companies ON DELETE CASCADE,
@@ -18,13 +14,11 @@ CREATE TABLE invoices (
     paid_date date,
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
-
 INSERT INTO companies
-  VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
-         ('ibm', 'IBM', 'Big blue.');
-
+VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
+    ('ibm', 'IBM', 'Big blue.');
 INSERT INTO invoices (comp_Code, amt, paid, paid_date)
-  VALUES ('apple', 100, false, null),
-         ('apple', 200, false, null),
-         ('apple', 300, true, '2018-01-01'),
-         ('ibm', 400, false, null);
+VALUES ('apple', 100, false, null),
+    ('apple', 200, false, null),
+    ('apple', 300, true, '2018-01-01'),
+    ('ibm', 400, false, null);
